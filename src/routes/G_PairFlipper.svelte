62b3cd93f88b5cb3c5faf95acee6c9140523151c
ray-pH <rayhan.azizi9@gmail.com>
2023-03-25 15:40:02 +0700

    pairFlipper : clickable card

--- src/routes/G_PairFlipper.svelte
+++ src/routes/G_PairFlipper.svelte
@@ -3,9 +3,10 @@
     import { onMount } from 'svelte';
     import { userstate, socketevent } from '../lib/types'
     import type { room, user, id, id_dict, color } from '../lib/types'
-    import type { gamestate, card } from '../games/pairFlipper'
+    import type { gamestate, card, gameinput } from '../games/pairFlipper'
     import { cardstate, cardopenstate } from '../games/pairFlipper'
 
+    export let userdata : user;
     export let f_updatescore : (uscore : id_dict<number>) => void;
     export let playercolors  : id_dict<color> = {};
 
@@ -16,15 +17,21 @@
 
     function get_color_from_cardstate(cs : cardstate) : color {
         let playernum : number = cardopenstate.indexOf(cs);
+        if (playernum < 0) return '#00000000';
         // TODO warn if playernum not exist
         let uid : id = gamestate.playerorder[playernum];
         return playercolors[uid];
     }
 
-    function get_cardlist(gs : gamestate) : {card : card, cardstate : cardstate}[] {
+    function get_cardlist(gs : gamestate) : {index : number, value : card, cardstate : cardstate}[] {
         let res = new Array(gs.board.length);
-        for (let i in gs.board) res[i] = {card : gs.board[i], cardstate : gs.boardstate[i]};
+        for (let i in gs.board) res[i] = {index : i, value : gs.board[i], cardstate : gs.boardstate[i]};
         return res;
+    }
+
+    function send_input(cardid : number){
+        let inp : gameinput = {uid : userdata.id, clicked_card : cardid};
+        io.emit(socketevent.GAME_INPUT, inp);
     }
 
     onMount(() => {
@@ -61,19 +68,24 @@
     #card-container {
         display: flex;
     }
+    .card-bg{
+        padding: 5px;
+        margin : 5px;
+    }
     .card {
         padding : 10px;
         background-color: white;
         color: black;
-        margin : 5px;
     }
 </style>
 
 <div id="main-container" class="main">
     <div id="card-container">
         {#each get_cardlist(gamestate) as cd}
-            <div class="card">
-                {cd.card}
+            <div class="card-bg" style="background-color:{get_color_from_cardstate(cd.cardstate)}">
+                <button class="card" on:click={() => {send_input(cd.index)}}>
+                    {cd.value}
+                </button>
             </div>
         {/each}
     </div>
