10ca6d0b920a328a3779ab8ab5150f505eb8d580
Rayhan Alghiffari Azizi <rayhan.azizi9@gmail.com>
2023-05-02 13:06:16 +0700

    change export in utils.ts and add arrayEq()

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -3,15 +3,20 @@ function randomAlphanum11() : string{
     if (res.length == 11) return res;
     return randomAlphanum11();
 }
-function genRandomAlphanum(len : number) : string{
+export function genRandomAlphanum(len : number) : string{
     if (len <= 11) return randomAlphanum11().substring(0,len);
     return randomAlphanum11() + genRandomAlphanum(len-11);
 }
-function shuffleArray(array : Array<any>) : void {
+export function shuffleArray(array : Array<any>) : void {
     for (let i = array.length - 1; i > 0; i--) {
         const j = Math.floor(Math.random() * (i + 1));
         [array[i], array[j]] = [array[j], array[i]];
     }
 }
 
-export { genRandomAlphanum, shuffleArray };
+export function arrayEq(a : ArrayLike<any>, b : ArrayLike<any>){
+    if (a.length != b.length) return false;
+    for (let i = 0; i < a.length; i++) 
+        if (a[i] != b[i]) return false;
+    return true;
+}
