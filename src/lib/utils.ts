b541f6dc2479f5bfef164d9d02e3448424999204
Rayhan Alghiffari Azizi <rayhan.azizi9@gmail.com>
2023-06-05 21:26:03 +0700

    utils: add reverse and improve arrayEq

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -19,9 +19,24 @@ export function shuffleArray(array : Array<any>) : void {
     }
 }
 
-export function arrayEq(a : ArrayLike<any>, b : ArrayLike<any>){
+/** return a copy of reversed array */
+export function reversed<T>(arr : Array<T>) : Array<T>{
+    let rev = new Array<T>(arr.length);
+    for (let i = 0; i < arr.length; i++) rev[i] = arr[arr.length-1-i];
+    return rev;
+}
+
+export function arrayEq<T>(a : Array<T>, b : Array<T>){
     if (a.length != b.length) return false;
     for (let i = 0; i < a.length; i++) 
         if (a[i] != b[i]) return false;
+    return true;
+}
+
+type equalityFunction<T> = (a : T, b : T) => boolean;
+export function arrayEqCustom<T>(a : Array<T>, b : Array<T>, f_equal : equalityFunction<T>){
+    if (a.length != b.length) return false;
+    for (let i = 0; i < a.length; i++) 
+        if (!f_equal(a[i], b[i])) return false;
     return true;
 }
